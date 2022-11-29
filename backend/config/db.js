2476f07936f9de693e445e80d5bce5e7de3582e0
Naresh <wdev6666@gmail.com>
2022-11-29 18:42:31 +0530

    Config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config({path: "backend/.env"});
 
 async function db() {
   const connection = await mongoose.connect(process.env.MONGO_URI);
